7148164353edcae54c30c7e0846fc99a340c73d0
anthoneyvalenzuela1 <149301770+anthoneyvalenzuela1@users.noreply.github.com>
2023-10-29 05:04:28 -0600

    Skonco chat (1) updates

--- src/app/child-view1/child-view1.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-child-view1',
-  templateUrl: './child-view1.component.html',
-  styleUrls: ['./child-view1.component.scss']
-})
-export class ChildView1Component {
-}
--- src/app/child-view2/child-view2.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-child-view2',
-  templateUrl: './child-view2.component.html',
-  styleUrls: ['./child-view2.component.scss']
-})
-export class ChildView2Component {
-}
